722c4674b3e3befbb2d50f464f33d76dff126b1b
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-07-06 10:42:08 +0200

    StudentDbX

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -12,7 +12,6 @@ public class Student {
         this.studentName = studentName;
         this.studentID = studentID;
     }
-
     public String getStudentName() {
         return this.studentName;
     }
